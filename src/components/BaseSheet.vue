2f29d94deadc1c824b9fcdd1941c9b4aec71a379
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-02-10 12:33:52 +0000

    feat: add BaseSheet component

---
+++ src/components/BaseSheet.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-full">
+        <div class="container mx-auto max-w-screen-lg">
+            <main id="wrapper" class="flex flex-col md:flex-row lg:m-12 shadow-2xl">
+                <div class="w-full md:max-w-xs p-8 bg-blue-50">
+                    <div class="px-2">
+                        <!-- @slot sidebar -->
+                        <slot name="sidebar"></slot>
+                    </div>
+                </div>
+                <div class="w-full p-12">
+                    <!-- @slot default -->
+                    <slot></slot>
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
