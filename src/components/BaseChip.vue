74c3386faa2a592128c51ee2ef37dbeec860b4be
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-02-10 12:32:38 +0000

    feat: add BaseChip component

---
+++ src/components/BaseChip.vue
@@ -0,0 +1,6 @@
+<template>
+    <span class="bg-blue-500 hover:bg-blue-400 text-white px-2 py-1 rounded-full m-0.5 text-sm">
+        <!-- @slot default -->
+        <slot></slot>
+    </span>
+</template>
