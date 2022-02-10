01d45e91ace06b56f51c24e65b47736e69fa15e9
Andres Janes <andres.janes@windsor-telecom.co.uk>
2022-02-10 12:34:14 +0000

    feat: add BaseDetailsSection component

---
+++ src/components/BaseDetailsSection.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2 class="text-lg font-semibold text-center mb-4">
+            <!-- @slot title -->
+            <slot name="title">Section</slot>
+        </h2>
+        <!-- @slot default -->
+        <slot></slot>
+    </div>
+</template>
